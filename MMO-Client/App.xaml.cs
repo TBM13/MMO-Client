11df2093182667cf1dc82cdc39bec45e11a744ff
TBM <mateo.trueba@gmail.com>
2021-06-21 15:24:59 -0300

    Delete mainWindow & move logger and loginscreen creation to app.xaml

--- MMO-Client/App.xaml.cs
+++ MMO-Client/App.xaml.cs
@@ -5,13 +5,29 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using MMO_Client.Common.Logger;
+using MMO_Client.Screens.LoginScreen;
 
 namespace MMO_Client
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        void App_Startup(object sender, StartupEventArgs args) => 
+            Setup();
+
+        private void Setup()
+        {
+            new Logger();
+
+            Logger.Info("Loading Login Screen", "Main");
+            LoginScreen loginScreen = new();
+            loginScreen.OnLoginEvent += InitiateLogin;
+            loginScreen.Show();
+        }
+
+        private void InitiateLogin(string user, string pass)
+        {
+
+        }
     }
 }
