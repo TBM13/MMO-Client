f8d94fada6c5fc47a0735bc3629c5ad4c78a2373
TBM <mateo.trueba@gmail.com>
2021-08-08 20:38:38 -0300

    Settings: Move JSON parse code to its own class

--- MMO-Client/Client/Config/Settings.cs
+++ MMO-Client/Client/Config/Settings.cs
@@ -1,6 +1,5 @@
 ﻿using MMO_Client.Client.Net;
 using System.Collections.Generic;
-using System.Text.Json;
 
 namespace MMO_Client.Client.Config
 {
@@ -34,7 +33,7 @@ namespace MMO_Client.Client.Config
             LoadFile lf = new();
             lf.OnFileLoaded += (data) =>
             {
-                Dictionary<string, dynamic> dic = ParseJson(data);
+                Dictionary<string, dynamic> dic = JsonUtils.ParseJsonDictionary(data);
                 foreach (KeyValuePair<string, dynamic> pair in dic)
                     Dictionary.Add(pair.Key, pair.Value);
 
@@ -45,57 +44,6 @@ namespace MMO_Client.Client.Config
                 OnSettingsLoaded?.Invoke(false);
 
             lf.Load(url);
-        }
-
-        private Dictionary<string, dynamic> ParseJson(string data)
-        {
-            JsonSerializerOptions options = new()
-            {
-                ReadCommentHandling = JsonCommentHandling.Skip
-            };
-
-            Dictionary<string, dynamic> dic = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(data, options);
-            foreach (KeyValuePair<string, dynamic> pair in dic)
-                dic[pair.Key] = ParseJsonElement(pair.Value);
-
-            return dic;
-        }
-
-        private dynamic ParseJsonElement(JsonElement elem)
-        {
-            switch (elem.ValueKind)
-            {
-                case JsonValueKind.False:
-                    return false;
-                case JsonValueKind.True:
-                    return true;
-                case JsonValueKind.String:
-                    return elem.GetString();
-                case JsonValueKind.Number:
-                    if (elem.TryGetInt32(out int i))
-                        return i;
-
-                    return elem.GetDouble();
-                case JsonValueKind.Object:
-                    return ParseJson(elem.GetRawText());
-                case JsonValueKind.Array:
-                    dynamic[] array = new dynamic[elem.GetArrayLength()];
-                    int i2 = 0;
-                    foreach(JsonElement e in elem.EnumerateArray())
-                    {
-                        array[i2] = ParseJsonElement(e);
-                        i2++;
-                    }
-
-                    return array;
-                case JsonValueKind.Null:
-                    return null;
-                case JsonValueKind.Undefined:
-                    return null;
-                default:
-                    Logger.Error($"Unsupported Json Element '{elem.ValueKind}'. Ignoring!");
-                    return null;
-            }
         }
     }
 }
---
+++ MMO-Client/Common/JsonUtils.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+
+namespace MMO_Client
+{
+    internal static class JsonUtils
+    {
+        public static Dictionary<string, dynamic> ParseJsonDictionary(string data)
+        {
+            JsonSerializerOptions options = new()
+            {
+                ReadCommentHandling = JsonCommentHandling.Skip
+            };
+
+            Dictionary<string, dynamic> dic = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(data, options);
+            foreach (KeyValuePair<string, dynamic> pair in dic)
+                dic[pair.Key] = ParseJsonElement(pair.Value);
+
+            return dic;
+        }
+
+        public static dynamic ParseJsonElement(JsonElement elem)
+        {
+            switch (elem.ValueKind)
+            {
+                case JsonValueKind.False:
+                    return false;
+                case JsonValueKind.True:
+                    return true;
+                case JsonValueKind.String:
+                    return elem.GetString();
+                case JsonValueKind.Number:
+                    if (elem.TryGetInt32(out int i))
+                        return i;
+
+                    return elem.GetDouble();
+                case JsonValueKind.Object:
+                    return ParseJsonDictionary(elem.GetRawText());
+                case JsonValueKind.Array:
+                    dynamic[] array = new dynamic[elem.GetArrayLength()];
+                    int i2 = 0;
+                    foreach (JsonElement e in elem.EnumerateArray())
+                    {
+                        array[i2] = ParseJsonElement(e);
+                        i2++;
+                    }
+
+                    return array;
+                case JsonValueKind.Null:
+                    return null;
+                case JsonValueKind.Undefined:
+                    return null;
+                default:
+                    Logger.Error($"Unsupported Json Element '{elem.ValueKind}'. Ignoring!");
+                    return null;
+            }
+        }
+    }
+}
