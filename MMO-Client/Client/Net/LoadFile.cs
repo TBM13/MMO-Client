ce0d5ebd33d096a2fd6d5606667f721b88734e3c
TBM <mateo.trueba@gmail.com>
2021-08-08 19:20:49 -0300

    Net: Implement basic "LoadFile"

---
+++ MMO-Client/Client/Net/LoadFile.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+
+namespace MMO_Client.Client.Net
+{
+    internal class LoadFile
+    {
+        public Events.String1Event OnFileLoaded { get; set; }
+        public Events.String1Event OnError { get; set; }
+
+        private readonly HttpClient client = new();
+
+        public async void Load(string url)
+        {
+            string data;
+            try
+            {
+                data = await client.GetStringAsync(url);
+            }
+            catch (HttpRequestException e)
+            {
+                Logger.Error($"Couldn't download file {url}");
+                Logger.Debug(e.ToString());
+                OnError?.Invoke(e.ToString());
+                return;
+            }
+
+            OnFileLoaded?.Invoke(data);
+        }
+    }
+}
