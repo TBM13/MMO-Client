4041a0bb0fa2b9c60054e1208733da59f3ecf428
TBM <mateo.trueba@gmail.com>
2021-08-08 20:28:57 -0300

    Main Window: Setup & load game settings

--- MMO-Client/MainWindow.xaml.cs
+++ MMO-Client/MainWindow.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MMO_Client.Client.Config;
+using MMO_Client.Screens;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace MMO_Client
 {
@@ -24,7 +14,76 @@ namespace MMO_Client
 
         private void Setup()
         {
+            // Setup Logger
             Logger.SetOutputScreen(LoggerOutput);
+
+            // Setup Game Settings
+            _ = new Settings();
+
+            // Setup Login Event
+            LoginScreen.OnLoginAttempt += OnLoginAttempt;
+        }
+
+        private void OnLoginAttempt(string username, string loginId, bool success)
+        {
+            if (!success)
+                return;
+
+            LoginScreen.OnLoginAttempt -= OnLoginAttempt;
+            LoginScreen.Visibility = Visibility.Hidden;
+
+            LoadScreen.Visibility = Visibility.Visible;
+            LoadGameSettings();
+        }
+
+        private void LoadGameSettings()
+        {
+            LoadScreen.ResetProgressbar();
+            LoadScreen.LabelText = "Loading Game Settings";
+
+            LoadScreen.OnRetryClick = null;
+            LoadScreen.OnRetryClick += LoadGameSettings;
+
+            LoadScreen.ResetProgressbar();
+
+            Settings.Instance.OnSettingsLoaded += (success) =>
+            {
+                Settings.Instance.OnSettingsLoaded = null;
+                if (!success)
+                {
+                    LoadScreen.ShowError("Couldn't load Game Settings", true);
+                    return;
+                }
+
+                LoadGameplaySettings();
+            };
+
+            Settings.Instance.LoadSettings();
+        }
+
+        private void LoadGameplaySettings()
+        {
+            LoadScreen.ResetProgressbar();
+            LoadScreen.LabelText = "Loading Gameplay Settings";
+
+            LoadScreen.OnRetryClick = null;
+            LoadScreen.OnRetryClick += LoadGameplaySettings;
+
+            LoadScreen.ResetProgressbar();
+
+            Settings.Instance.OnSettingsLoaded += (success) =>
+            {
+                Settings.Instance.OnSettingsLoaded = null;
+                if (!success)
+                {
+                    LoadScreen.ShowError("Couldn't load Gameplay Settings", true);
+                    return;
+                }
+
+                LoadScreen.Visibility = Visibility.Hidden;
+            };
+
+            Settings.Instance.LoadGameplaySettings();
         }
     }
 }
