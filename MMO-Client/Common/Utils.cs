9517c104dc7b1fb215ae18b5d89915ffd6149c9d
TBM <mateo.trueba@gmail.com>
2021-08-08 21:46:04 -0300

    Implement Utils class

---
+++ MMO-Client/Common/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace MMO_Client
+{
+    internal static class Utils
+    {
+        private readonly static BrushConverter brushConverter = new();
+
+        public static long GetUnixTime() =>
+            DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+
+        public static Brush BrushFromHex(string hex) =>
+            (Brush)brushConverter.ConvertFromString(hex);
+    }
+}
