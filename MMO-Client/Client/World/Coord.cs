f33c850cdb12fe4dab5b4ed3505bd5b3d83fb5a1
TBM <mateo.trueba@gmail.com>
2021-08-10 20:15:46 -0300

    World: Coord: Implement ToArray()

--- MMO-Client/Client/World/Coord.cs
+++ MMO-Client/Client/World/Coord.cs
@@ -6,6 +6,8 @@
         public int Y { get; init; }
         public int Z { get; init; }
 
+        private int[] array;
+
         public Coord(int x = 0, int y = 0, int z = 0)
         {
             X = x;
@@ -18,6 +20,16 @@
             X = array[0];
             Y = array[1];
             Z = array[2];
+        }
+
+        public int[] ToArray()
+        {
+            array ??= new int[3];
+            array[0] = X;
+            array[1] = Y;
+            array[2] = Z;
+
+            return array;
         }
     }
 }
