f3b8be4c524bc30741c83ba8429cad27fd00368c
TBM <mateo.trueba@gmail.com>
2021-08-10 18:23:47 -0300

    Attributes: Implement Custom Attribute List

---
+++ MMO-Client/Client/Attributes/CustomAttributeList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace MMO_Client.Client.Attributes
+{
+    internal class CustomAttributeList
+    {
+        private readonly Dictionary<string, CustomAttribute> attributes = new();
+
+        public CustomAttributeList() { }
+
+        public object GetValue(string key)
+        {
+            if (attributes.ContainsKey(key))
+                return attributes[key].Value;
+
+            Logger.Warn($"Requested a non-existent attribute '{key}'");
+            return null;
+        }
+
+        public void SetValue(string key, object value) => 
+            attributes[key].Value = value;
+
+        public void Clear() =>
+            attributes.Clear();
+    }
+}
