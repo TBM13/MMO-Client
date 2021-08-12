4517a5c06b5ccbe10e921ddac24d0c6b761c23c8
TBM <mateo.trueba@gmail.com>
2021-08-12 18:40:41 -0300

    Net: Implement ChangeRoomActionRequest

---
+++ MMO-Client/Client/Net/Requests/Room/ChangeRoomActionRequest.cs
@@ -0,0 +1,16 @@
+﻿using MMO_Client.Client.Net.Mines;
+using MMO_Client.Client.World.Rooms;
+
+namespace MMO_Client.Client.Net.Requests.Room
+{
+    internal class ChangeRoomActionRequest : MamboRequest
+    {
+        private readonly RoomLink link;
+
+        public ChangeRoomActionRequest(RoomLink link) : base("ChangeRoomActionRequest") => 
+            this.link = link;
+
+        public override Mobject Build() => 
+            link.ToMobject();
+    }
+}
